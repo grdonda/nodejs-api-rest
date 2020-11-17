e786c7012dcb7136e97d886bbdcda14a8675bfe4
Giovanni Donda <grdonda@gmail.com>
2020-11-17 11:38:02 -0300

    testes para obter os dados pela rota get

--- app/core/products/__tests__/products.test.js
+++ app/core/products/__tests__/products.test.js
@@ -1,1 +1,14 @@
-// teste
+const supertest = require("supertest");
+const app = require("../../../../api");
+const request = supertest(app);
+
+describe("GET /products", () => {
+
+  it("should to list products", async () => {
+    const res = await request.get("/products");
+
+    expect(res.status).toBe(200);
+    expect(Array.isArray(res.body)).toBe(true);
+  });
+  
+});
